6b7ee5dd05fe05b54078da53c15bc4fa3a216bcd
jafmoralesss <jafetmoralesmartinez@gmail.com>
2025-10-02 14:32:46 -0600

    Update/ Added an ApiClient in model. It just 'writes' the url for SerpApi

---
+++ AuthorSeeker/src/main/java/org/example/Model/ApiClient.java
@@ -0,0 +1,25 @@
+package org.example.Model;
+
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
+
+public class ApiClient {
+
+    private final String apiKey = System.getenv("serpApiKey");
+
+    public void displayUrl (String queryText) {
+
+        String encodedQuery = URLEncoder.encode(queryText, StandardCharsets.UTF_8);
+        String base = "https://serpapi.com/search.json";
+        String completeUrl = base
+                + "?engine=google_scholar"
+                + "&q=" + encodedQuery
+                + "&hl=en"
+                + "&num=5"
+                + "&start=0"
+                + "&api_key=" + apiKey;
+
+        System.out.println("URL: " + completeUrl);
+    }
+}
