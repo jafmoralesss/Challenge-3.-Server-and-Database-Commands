6243f3fce57bf840645cca42104b82bdda00679e
jafmoralesss <jafetmoralesmartinez@gmail.com>
2025-10-02 17:08:45 -0600

    Update/ ApiClient uses a GET() to request JSON content form SerpApi

--- AuthorSeeker/src/main/java/org/example/Model/ApiClient.java
+++ AuthorSeeker/src/main/java/org/example/Model/ApiClient.java
@@ -1,8 +1,16 @@
 package org.example.Model;
 
+import java.io.IOException;
+import java.net.URI;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
-import java.util.Scanner;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
 
 public class ApiClient {
 
@@ -22,5 +30,31 @@ public class ApiClient {
                 + "&api_key=" + apiKey;
 
         System.out.println("URL: " + completeUrl);
+
+        HttpGet request = new HttpGet(completeUrl);
+
+        try (CloseableHttpClient httpClient = HttpClients.createDefault();
+        CloseableHttpResponse response = httpClient.execute(request)) {
+
+            HttpEntity entity = response.getEntity();
+
+            int statusCode = response.getStatusLine().getStatusCode();
+
+            if (statusCode == 200){
+
+                if (entity != null){
+                    String jsonBody = EntityUtils.toString(entity);
+
+                    System.out.println("Status: " + statusCode);
+                    System.out.println("JSON body" + jsonBody);
+
+
+                }
+            } else {
+                System.err.println("API call failed. Status: " + statusCode);
+            }
+        }  catch (IOException e){
+            System.err.println("Connection error for this request: " + e.getMessage());
+        }
     }
 }
